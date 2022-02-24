8653141e7029c8e36ecae699d3355abbdfcfd8f4
Arsen <lisnychukarsenw@gmail.com>
2022-02-24 13:32:37 +0200

    add unit test

---
+++ UnitTest/UnitTest.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\Lab_12_1.3\Rational.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+		TEST_METHOD(TestMethod1)
+		{
+			Rational r1;
+			r1.Init(15, 3);
+			Rational r2;
+			r2.Init(2, 1);
+			Assert::AreEqual(div(r1, r2).getA(), 5);
+		}
+	};
+}
